4281d506c83269ed02cba8492c949a493765714d
Will Spurr <willjspurr@gmail.com>
2019-02-11 20:38:21 -0600

    everything working but cd

---
+++ README.txt
@@ -0,0 +1,34 @@
+William Spurr
+
+==== Submission Files ====
+
+shell.c
+- source code for the shell
+
+README
+- this file
+
+==== Compiling Program and Running ====
+	You can compile my shell program by typing the command
+'gcc -o shell shell.c'
+
+	To run the shell program, type ./shell from the directory you placed
+the binary file in. Make sure it is executable by and use the command
+'chmod a+x shell' if needed.
+
+
+==== Challenges ====
+
+	The first challenge I encountered was how to remove a trailing '\n'
+from a string gotten from stdin by fgets. I found a solution at
+https://stackoverflow.com/questions/2693776/removing-trailing-newline-character-from-fgets-input using the function strcspn.
+
+	I had a lot of problems using strtok to tokenize me input strings so
+execvp could read them properly. I eventually fixed it, but I'm not entirely
+sure how it was all fixed.
+	I used https://www.geeksforgeeks.org/pass-2d-array-parameter-c/ to learn
+how to pass an array of pointers to strings to a function. I was then having
+issues where strtok wasn'y returning me any tokenized output past the first
+token, or else I didn't know how to read it. I modified my strtok call loop
+with help from https://stackoverflow.com/questions/15472299/split-string-into-tokens-and-save-them-in-an-array . I'm not really sure how, but
+simplifying my loop using the example from that post made everything work.
--- shell.c
+++ shell.c
@@ -1,3 +1,8 @@
+/* shell.c - William Spurr - 3 Feb 2019
+ * This program is a basic command line shell written for Operating Systems
+ * class. It lets you run programs in new processes, show the last 5 created
+ * process IDs, and use cd. */
+
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
@@ -9,12 +14,13 @@
 
 void tok_str_cmd(char *tok_cmd[TOKEN_COUNT + 1], char *source);
 
+void add_recent_num(int array[5], int pid, int array_size);
 
 
 int main () {
 
 	char *prompt = "prompt$ "; //TODO make this dynamic with pwd
-
+	int recent_pids[5] = {0}; // 5 most recent pids created
 	// main execution and input loop
 	while (1) {
 		// init input string and get a user input
@@ -23,22 +29,26 @@ int main () {
 		fgets(input, TOKEN_COUNT * TOKEN_SIZE, stdin);
 		// remove trailing '\n'
 		input[strcspn(input, "\n")] = '\0';
-		printf("the input was %s\n", input); // TODO debug delete
 
 		// test for different inputs
 		// 'exit' shell -> break loop and quit
 		if (strncmp(input, "exit", 4) == 0) {
 			break;
+		} 
+
+		// 'showpid' to show most recent child pids created
+		else if (strncmp(input, "showpid", 7) == 0) {
+			for (int i = 0; i < sizeof(recent_pids) / sizeof(recent_pids[0]); i++) {
+			printf("%i\n", recent_pids[i]);
+			}
 		}
-		// TODO add checks for showpid and cd
+
+		// TODO add check for cd
 		
 		// command -> fork -> execvp & waitpid -> return
 		else {
 			pid_t pid = fork();
-			if (pid == 0) {
-				printf("I am child process with pid %i\n", pid);
-			
-							
+			if (pid == 0) { // child process runs new command
 				// create strings to hold command and arguments
 					// array of pointers to strings, w/ TOKEN_COUNT strings
 					// size + 1 for NULL pointer terminator for execvp()
@@ -46,47 +56,18 @@ int main () {
 				// separate command and args
 				tok_str_cmd(tok_cmd, input); 
 				
-
-				// TOKENIZE
-				/*								
-				printf("tokenizing string: %s\n", input); // TODO debug del
-				const char s[2] = " ";
-				int tok_cnt = 0;
-	
-				// get first token
-				char *tok = strtok(input, s);
-	
-				//strcpy(tok_cmd[tok_cnt], tok);
-				tok_cmd[tok_cnt] = tok;
-
-				while (tok_cmd[tok_cnt] != NULL) {
-					printf("in loop");
-					printf("tokenized: %s\n", tok_cmd[tok_cnt]); // TODO debug del
-					tok = strtok(NULL, s);
-					tok_cmd[++tok_cnt] = tok;
-				}
-				*/
-				// TOKENIZE
-
 				// execute new process by replacing this child
 				execvp(tok_cmd[0], tok_cmd);
 				
-					
-				/*char const* mycmd[4];
-				mycmd[0] = "ls";
-				mycmd[1] = "-al";
-				mycmd[2] = "/";
-				mycmd[3] = NULL;
-				execvp(mycmd[0], mycmd);
-				*/
-				
 				// child should not reach here, something happened in error
-				printf("Something happened!\n");
+				printf("Command not found\n");
 				return 0;
 			}
-			else {
+			else { // parent process waits
 				waitpid(-1, NULL, 0);
-				printf("I am parent process with child pid %i and am done waiting\n", pid);
+
+				// add child pid to list of recent ones
+				add_recent_num(recent_pids, pid, sizeof(recent_pids) / sizeof(recent_pids[0]));
 			}
 		} // END command if statement
 		
@@ -101,22 +82,33 @@ int main () {
  * command. */
 void tok_str_cmd(char *tok_cmd[TOKEN_COUNT + 1], char *source) {
 
-				printf("tokenizing string: %s\n", source); // TODO debug del
-				const char s[2] = " ";
-				int tok_cnt = 0;
+	const char s[2] = " ";
+	int tok_cnt = 0;
 	
-				// get first token
-				char *tok = strtok(source, s);
+	// get first token
+	char *tok = strtok(source, s);
 	
-				//strcpy(tok_cmd[tok_cnt], tok);
-				tok_cmd[tok_cnt] = tok;
+	//strcpy(tok_cmd[tok_cnt], tok);
+	tok_cmd[tok_cnt] = tok;
 
-				while (tok_cmd[tok_cnt] != NULL) {
-					printf("in loop");
-					printf("tokenized: %s\n", tok_cmd[tok_cnt]); // TODO debug del
-					tok = strtok(NULL, s);
-					tok_cmd[++tok_cnt] = tok;
-				}
+	// loop through rest of tokens and add to list
+	while (tok_cmd[tok_cnt] != NULL) {
+		tok = strtok(NULL, s);
+		tok_cmd[++tok_cnt] = tok;
+	}
 }	// END tok_str_cmd()
 
 //void tok_str_cd(); // TODO
+
+/* Adds pid to an array of integers. Puts new num in the last position.
+ * Deletes the first num. */
+void add_recent_num(int array[], int num, int array_size) {
+	
+	// loop through all array elements except last
+	for (int i = 0; i < array_size - 1; i++) {
+		array[i] = array[i+1];
+	} // END for
+
+	// set last element
+	array[array_size - 1] = num;
+} // END add_recent_num()
